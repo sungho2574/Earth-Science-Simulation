3529585c98a91e2a8416d52b31f8e73edc73fe23
sungho2574 <96642366+sungho2574@users.noreply.github.com>
2021-12-26 15:02:47 +0900

    update: upload code from GlowScript

--- solar system.py
+++ solar system.py
@@ -1,59 +1,105 @@
 #GlowScript 3.2 VPython
 
-
-
-from numpy import datetime_data
 from vpython import *
 from vpython.no_notebook import stop_server
 
+scene.width  = 1000
+scene.height = 400
+scene.title = "<b>tangential v & radial v\n\n</b>"
 
+G = 6.7e-11
+
+
+
+# sun / planets
+sun   = sphere(pos=vec(0, 0, 0), radius=2e10, mass=2e30, color=color.red)
+
+PLANET_NUM = 5
+planets = []
+
+x = [1e11, 1.8e11, 3.4e11, 4.1e11, 5.5e11]
+
+r = [0.6e10, 0.5e10, 0.7e10, 1.5e10, 0.7e10]
+
+m = [1e30, 5e30, 10e30, 1e10, 10e10]
+
+v = [3e4, 2e4, 1.4e4, 1.6e4, 1.7e4]
+
+for i in range(PLANET_NUM):
+    planets.append(sphere(pos=vec(x[i], 0, 0), radius=r[i], mass=m[i], p=vec(0, 0, -v[i])*m[i], color=color.white, make_trail = True))    
+
+
+
+# arrow
+radial_arrow = arrow(pos=vec(0, 0, 0), axis=vec(0, 0, 0), shaftwidth=0.02e11, headwidth=2.5e10, headlength=4e10, color=color.yellow)
+
+
+
+
+# Exit
+# ===============================================================================
 exit = False
 
-def B(b):
-    #stop_server()
+def ex(b):
     global exit
     exit = True
     print(exit)
     return
 
-button( bind=B, text='Click me!' )
-
-G = 6.7e-11
-
-sun = sphere(pos=vec(0, 0, 0), radius=2e10, mass=2e30, color=color.red)
+button(text='Exit', pos=scene.title_anchor, bind=ex)
 
 
 
-PLANET_NUM = 1
-planets = []
 
-for i in range(PLANET_NUM):
-    planets.append(sphere(pos=vec((random()+i+1)*1e11, 0, 0), radius=0.5e10, mass=1e30, p=vec(0, 0, random()*-1e4), color=color.white, make_trail = True))
+# Run / Pause
+# ===============================================================================
+running = True
+
+def run(b):
+    global running
+    
+    running = not running
+    
+    if running:
+        b.text = "Pause"
+    
+    else: 
+        b.text = "Run"
+
+button(text="Pause", pos=scene.title_anchor, bind=run)
 
 
 
-dt = 1e5
-while True:
-    rate(500)
 
-    for p in planets:    
-        r = sun.pos - p.pos 
-        F = G * sun.mass * p.mass * r.hat / mag2(r)
+# speed
+# ===============================================================================
+MIN = 5
+MAX = 200
+VAL = 10
+speed = VAL
 
-        p.p += F*dt
-        p.pos += (p.p/p.mass) * dt
+scene.append_to_caption('\nspeed: ')
+
+def set_speed (s):
+    global speed
+    speed = s.value
+
+slider(min=MIN, max=MAX, value=VAL, length=220, bind=set_speed, right=15)
+
+scene.append_to_caption('\n\n')
 
 
-    if (exit == True):
-        #stop_server()
-        break
+
 
 # label
 # ===============================================================================
-start_checked  = 0
-arrive_checked = 0
+start_checked = 0
+end_checked   = 0
 
-def set_start ():
+start_planet = 0
+end_planet   = 0
+
+def set_start (r):
     global chb_start
     global start_checked
 
@@ -73,26 +119,41 @@ def set_start ():
         if c:
             start_checked = i
 
-def set_arrive ():
-    global chb_arrive
-    global arrive_checked
+def set_end ():
+    global chb_end
+    global end_checked
 
     checked = []
-    for c in chb_arrive:
+    for c in chb_end:
         if c.checked:
             checked.append(True)
         
         else:
             checked.append(False)
     
-    checked[arrive_checked] = False
+    checked[end_checked] = False
 
     for i, c in enumerate(checked):
-        chb_arrive[i].checked = c
+        chb_end[i].checked = c
         
         if c:
-            arrive_checked = i
+            end_checked = i
 
+def set_aim (b):
+    global start_planet
+    global end_planet
+
+    start_planet = start_checked
+    end_planet   = end_checked
+
+    global time
+    time = 0
+    
+    g1.delete()
+    g2.delete()
+
+
+# start
 scene.append_to_caption('start:\t\t')
 
 chb_start = []
@@ -103,36 +164,79 @@ chb_start[0].checked = True
 scene.append_to_caption('\n\n')
 
 
-scene.append_to_caption('arrive:\t\t')
+# end
+scene.append_to_caption('end:\t\t\t')
 
-chb_arrive = []
+chb_end = []
 for i in range(5):
-    chb_arrive.append(radio(text='{:}        '.format(i+1), checked=False, bind=set_arrive))
+    chb_end.append(radio(text='{:}        '.format(i+1), checked=False, bind=set_end))
 
-chb_arrive[0].checked = True
-scene.append_to_caption('\n\n')
+chb_end[0].checked = True
 
-
-
-button(text='Apply', bind=ex)
-
-scene.append_to_caption('\n\n')
+button(text='Apply', bind=set_aim)
+scene.append_to_caption('\n\n\n')
 
 
 
-start_planet = 1
-end_planet = 1
 
-
+# graph
+# ===============================================================================
 def rt_v ():
-    s = planets[start_checked].p / planets[start_checked].mass
-    e = planets[start_checked].p / planets[arrive_checked].mass
+    s = planets[start_planet].p / planets[start_planet].mass
+    e = planets[end_planet].p / planets[end_planet].mass
+    r_axis = planets[end_planet].pos - planets[start_planet].pos        # radial velocity basis axis
 
-    v = e - s       # relative velocity
+    v = e - s                                               # relative velocity
+    
+    radial_v = dot(v, r_axis.norm())
 
-    axis = planets[arrive_checked].pos - planets[start_checked].pos
+    t_axis = vec(-r_axis.z, 0, r_axis.x)                    # tangential velocity basis axis (ccw)
+    tangential_v = v - radial_v*r_axis.norm()          
+    tangential_v = dot(tangential_v, t_axis.norm())         # caculate t_v on t_axis
 
-    radial_v     = dot(v, axis.norm())
-    tangential_v = v - radial_v*axis.norm()
+    return tangential_v, radial_v
 
-    return radial_v, tangential_v
+time = 0
+
+f1 = graph(width=600, height=200, title='tangential velocity')
+g1 = gcurve(graph=f1, color=color.red)
+
+f2 = graph(width=600, height=200, title='radial velocity')
+g2 = gcurve(graph=f2, color=color.blue)
+
+
+
+
+# ===============================================================================
+#                                     main
+# ===============================================================================
+dt = 1e5
+while True:
+    rate(speed)
+
+    if running:
+        for p in planets:
+            r = sun.pos - p.pos 
+            F = G * sun.mass * p.mass * r.hat / mag2(r)
+
+            p.p += F*dt
+            p.pos += (p.p/p.mass) * dt
+        
+
+        # arrow
+        radial_arrow.pos  = planets[start_planet].pos
+        radial_arrow.axis = planets[end_planet].pos - planets[start_planet].pos
+
+
+        # graph
+        tv, rv = rt_v()
+        
+        g1.plot(time, tv)
+        g2.plot(time, rv)
+        
+        time += 1
+
+    if (exit == True):
+        break
+
+stop_server()
